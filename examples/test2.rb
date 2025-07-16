81a02f034b8bccae79412ad0315bb0eec0e47fe3
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-07-16 16:18:01 +0300

    debug: landing host correctly gets updated after previous use

--- examples/test2.rb
+++ examples/test2.rb
@@ -1,26 +1,17 @@
 #!/usr/bin/ruby
 
-puts "Starting on #{`hostname`}"
-
 def hw_info
-  puts "Scanning #{`hostname`.strip}"
+  puts "scanning #{`hostname`.strip}..."
   `cat /proc/cpuinfo  | grep processor | wc -l`.to_i
 end
-
-cores = 0
-
-cores += hw_info
-
-puts cores
 
 
 
 require_relative '../sources/global-ruby'
 
+hosts = [ '130.162.50.40', '127.0.0.1' ]
+cores = 0
 
-Global.land(binding, :hw_info, '130.162.50.40') # TODO: needed, but there must be default behaviour, landing to automatically picked node (maybe shape specified, too)
+hosts.each { |h| Global.land(binding, :hw_info, h) and cores += hw_info }
 
-# The hub must be activated before the initial point of entry of the SW project. It's that simple.
-cores += hw_info
-
-puts cores
+puts "Total cores on all hosts: #{cores}"
